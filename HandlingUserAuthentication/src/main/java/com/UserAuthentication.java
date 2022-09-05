e8ce02ee752a45738bfd84e367653dad0e3e8471
kampallinaveena <105041654+kampallinaveena@users.noreply.github.com>
2022-09-05 17:49:48 +0530

    "Changes have been committed"

---
+++ HandlingUserAuthentication/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "ABC";
+	}
+	public String getEmail() {
+		return "abc@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456789;
+	}
+	public String getPassword() {
+		return "abc@123";
+	}
+}
